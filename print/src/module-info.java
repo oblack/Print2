1dcc9d0acf594f950f97b27d0e3c10baab884b0a
Orlin Genchev <ogenchev@gmail.com>
2018-08-27 19:30:50 +0300

    Print library 2 - without an argument

---
+++ print/src/module-info.java
@@ -0,0 +1,3 @@
+module print {
+    exports print;
+}
---
+++ print/src/print/Print.java
@@ -0,0 +1,8 @@
+
+package print;
+
+public class Print {
+    public void print() {
+        System.out.println("No arguments print");
+    }
+}
